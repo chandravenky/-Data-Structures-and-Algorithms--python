ad39da953bd393855d8eed510962e7d77e80c066
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2021-08-28 15:47:09 +0000

    implement set and get

--- main.py
+++ main.py
@@ -1,9 +1,50 @@
+class Hashtable:
+    
+    def __init__(self, size):
+        self.data = [None]*size
+ 
+    def __str__(self):
+        return str(self.__dict__)
+  
+    def _hash(self, key):
+        hash = 0
+        i = 0
+        while i < len(key):
+          hash = (hash + ord(key[i]) * i) % len(self.data)
+          i += 1
+        return hash
+    
+    def set(self, key, value):
+
+        address = self._hash(key)
+        
+        if not self.data[address]:
+            
+            self.data[address] = []
+        
+        self.data[address].append([key, value])
+
+        return self.data
+
+    def get(self, key):
+    
+        address = self._hash(key)
+
+        if self.data[address]:
+            
+            for  [stored_key,value] in self.data[address]:
+                
+                if stored_key == key:
+                    
+                    return value
+            
+        return None
 
 
-def findNemoo(list1):
+	
 
-  for i in range(0, len(list1)):
-    if list1[i] == 'Nemo':
-      print("Nemo found")
 
-findNemoo(['Nemo'])
+
+
+
+
