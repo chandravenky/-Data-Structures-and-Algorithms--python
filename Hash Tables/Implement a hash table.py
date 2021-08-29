b0d18efa9382f57eaa242e10261ac90913395e12
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2021-08-29 16:57:58 +0000

    prepend corrected

--- Hash Tables/Implement a hash table.py
+++ Hash Tables/Implement a hash table.py
@@ -1,99 +1,74 @@
-class Hashtable:
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 29 09:42:57 2021
+
+@author: venkatesh.chandra
+"""
+
+
+class Node():
     
-    def __init__(self, size):
-        self.data = [None]*size
- 
+    def __init__(self, data):    
+        self.data= data
+        self.next = None
+    
+
+class LinkedList():
+
+    def __init__(self, value):
+        
+      self.head = Node(value)
+      self.tail = self.head
+      self.length = 1
+      
     def __str__(self):
         return str(self.__dict__)
-  
-    def _hash(self, key):
-        hash = 0
+    
+    def append(self, data):
+        new_node = Node(data)
+        self.tail.next = new_node
+        self.tail = new_node
+        self.length = self.length +1
+    
+    def prepend(self, data):
+        
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+        self.length = self.length +1
+    
+    def print_list(self):
+        temp = self.head
         i = 0
-        while i < len(key):
-          hash = (hash + ord(key[i]) * i) % len(self.data)
-          i += 1
-        return hash
-    
-    def set(self, key, value):
-
-        address = self._hash(key)
+        while i < self.length:
+            print(temp.data, end='-->')
+            temp = temp.next
+            i += 1
+        print()
         
-        if not self.data[address]:
-            
-            self.data[address] = []
-        
-        self.data[address].append([key, value])
+    def print1(self):
 
-        return self.data
+        temp = self.head
 
-    def get(self, key):
-    
-        address = self._hash(key)
+        while temp != None:
 
-        if self.data[address]:
-            
-            for  [stored_key,value] in self.data[address]:
-                
-                if stored_key == key:
-                    
-                    return value
-            
-        return None
+            print(temp.data, end = ' ')
 
+            temp = temp.next
 
-    def  keys(self):
-      
-        keys_result = []
-        
-        for item in self.data:
-            
-            if item:
-                
-                for key,_ in item:
-                    
-                
-                    keys_result.append(key)
-      
-        return keys_result
+        print()
 
+        print("Length = " + str(self.length))
 
-    def remove(self, key):
-        
-        address = self._hash(key)
-        
-        if self.data[address]:
-        
-            for i in range(0, len(self.data[address])):
-                
-                if self.data[address][i][0] == key:
-                    
-                    self.data[address].pop(i)
-                    
-            if self.data[address] == []:
-
-                self.data[address] = None
-                        
-        return None
-	
-
-
-
-'''
-h = Hashtable(50)
-h.set('grapes', 10000)
-h.set('grapes', 20000)
-print(h.get('grapes'))
-print(h.keys())
-'''
-
-k = Hashtable(50)
-k.set('grapes', 10000)
-k.set('apples', 20000)
-print(k)
-print(k.get('grapes'))
-print(k.get('apples'))
-print(k.keys())
-print(k.remove('apples'))
-print(k)
-
+myLL = LinkedList(10)
+myLL.prepend(7)
+myLL.print1()
+myLL.append(10)
+myLL.print1()
+myLL.append(20)
+myLL.print1()
+myLL.prepend(5)
+myLL.print1()
+myLL.append(25)
+myLL.print1()
 
