2c77e7cf8ebc41d8a41b6d35c9571d51d3c905b9
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2021-08-29 16:19:45 +0000

    prepend method

--- Linked List/Implement a linked list LL.py
+++ Linked List/Implement a linked list LL.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 29 09:42:57 2021
+
+@author: venkatesh.chandra
+"""
+
+
 class Node():
     
     def __init__(self, data):    
@@ -22,7 +30,20 @@ class LinkedList():
             self.head.next = Node(data)
             self.tail = Node(data)
             self.length = self.length +1
-            
+    
+    def prepend(self, data):
+        
+        if self.head == None:
+            self.head = Node(data)
+            self.tail = Node(data)
+            self.length = 1
+        
+        else:
+            temp_node = self.head
+            self.head = Node(data)
+            self.head.next = temp_node
+            self.length = self.length +1
+    
     def print_list(self):
         temp = self.head
         i = 0
@@ -38,3 +59,7 @@ myLL.append(10)
 myLL.print_list()
 myLL.append(20)
 myLL.print_list()
+myLL.prepend(5)
+myLL.print_list()
+
+
