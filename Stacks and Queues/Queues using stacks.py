0bf3d3c7063c6faa5af29eb4af214a5782299fc0
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2021-10-18 10:54:38 -0600

    implement pop

--- Stacks and Queues/Queues using stacks.py
+++ Stacks and Queues/Queues using stacks.py
@@ -14,9 +14,13 @@ class MyQueue(object):
         
 
     def pop(self):
-        """
-        :rtype: int
-        """
+        if len(self.first)>0:
+          return self.first.pop()
+        length = len(self.last)
+        for i in range(0, length):
+          self.first.append(self.last.pop())
+        
+        return self.first.pop()
         
 
     def peek(self):
@@ -32,10 +36,14 @@ class MyQueue(object):
         
 
 
-# Your MyQueue object will be instantiated and called as such:
 obj = MyQueue()
 obj.push("Jerry")
+print(obj.first)
+print(obj.last)
 obj.push("Sherry")
+print(obj.first)
+print(obj.last)
+print(obj.pop())
 print(obj.first)
 print(obj.last)
 
